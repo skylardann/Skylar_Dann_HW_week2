01d29c53236089fe0433b8619f83fa3ad320ed4a
skylardann <skylardann@cca.edu>
2017-01-29 15:24:45 -0800

    Create Sketch-3.js

---
+++ Sketch-3.js
@@ -0,0 +1,17 @@
+/// Final 3 
+
+
+
+function setup() {
+	frameRate(30);
+  createCanvas(600, 600);  
+}
+
+function draw() {
+	background (250);
+	
+	var posX = 100 + (frameCount * 10); 
+	
+	ellipse (posX,100,50,50)
+	
+}
